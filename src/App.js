fea93efa15900ed04a3b2ff0bc31a723e181f67f
sarahrourke <serourke@gmail.com>
2021-01-17 02:53:26 -0600

    refactor for cleanup and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { Table, Image } from 'react-bootstrap';
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {/* contacts table */}
+      <div className="table-container">
+        <Table striped bordered hover>
+        
+          <thead className="text-muted">
+            <tr>
+              <th className="text-center head-row">
+                <div className="form-check">
+                  <input className="form-check-input position-static" type="checkbox" value="" id="defaultCheck1" aria-label="..."></input>
+                </div>
+              </th>
+              <th>Contact</th>
+              <th>Total Value</th>
+              <th>Location</th>
+              <th className="text-center">Deals</th>
+              <th>Tags</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td className="text-center">
+                <div className="form-check">
+                  <input className="form-check-input position-static" type="checkbox" value="" id="defaultCheck1" aria-label="..."></input>
+                </div>
+              </td>
+              <td>
+                <Image src="..." roundedCircle thumbnail />
+                Contact First Name
+                Contact Last Name
+              </td>
+              <td>
+                $Total Value
+              </td>
+              <td>
+                City, State, Country
+              </td>
+              <td className="text-center">
+                Deals - number of 
+              </td>
+              <td>
+                Tags
+              </td>
+            </tr>
+          </tbody>
+
+        </Table>
+      </div>
+
     </div>
   );
 }
