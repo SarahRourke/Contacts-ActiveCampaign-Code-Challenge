201ce1d58524eb847cb2cbe35043e23733380c69
sarahrourke <serourke@gmail.com>
2021-01-17 23:42:35 -0600

    congif cors

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -22,8 +22,8 @@ class Contacts extends Component {
             headers: {
                 //this env variable will be set during deploykent so apiKey is not bundled with rest of app code
                 'Api-Token': process.env.REACT_APP_API_TOKEN,
-                // 'Access-Control-Allow-Origin': 'https://shielded-spire-74256.herokuapp.com',
-                'Cache-Control': 'max-age=120'
+                'Access-Control-Allow-Origin': 'https://shielded-spire-74256.herokuapp.com',
+                'Cache-Control': 'max-age=120 public max-stale[=300]'
             },
         })
         .then(res => {
