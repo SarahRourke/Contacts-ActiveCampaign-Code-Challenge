d9c215d119c9c89aacf81006dffab3562e217c2c
sarahrourke <serourke@gmail.com>
2021-01-25 10:11:04 -0600

    .

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -9,7 +9,7 @@ const Tag = (props) => {
     const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
-        axios.get(`https://cors-anywhere.herokuapp.com/sahmed93846.api-us1.com/api/3/tags/${props.props}?limit=1`
+        axios.get(`https://cors-anywhere.herokuapp.com/sahmed93846.api-us1.com/api/3/tags/${props.props}?limit=0`
         , {
             headers: {
                 'Api-Token' : process.env.REACT_APP_API_TOKEN,
@@ -20,7 +20,7 @@ const Tag = (props) => {
         .then(data => {
             setTag(data.tag)
             setLoaded(true)
-            console.log(data)
+        
         })
         .catch(error => console.log(error))
     }, [])
